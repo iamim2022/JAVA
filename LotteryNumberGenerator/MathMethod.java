08123ba5c97d6b51b09321aa04c8e1daa480cc36
iamim2022 <procom7612@gmail.com>
2022-03-30 00:03:31 +0900

    MathMethod

---
+++ LotteryNumberGenerator/MathMethod.java
@@ -0,0 +1,12 @@
+package Lottery_number_Generator;
+
+public class MathMethod {
+	public static void main(String[] args) {
+		System.out.println(Math.min(3, 13));
+		System.out.println(Math.max(123, 54));
+		System.out.println(Math.abs(-23.1));
+		System.out.println(Math.sqrt(3));
+		System.out.println(Math.random());
+	}
+}
+
